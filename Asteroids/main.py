7d46c260d5bd6ce668589183b858d42a3052b0b9
LucaLamberti02 <luca.lamberticode@gmail.com>
2024-11-19 03:33:16 +0100

    A:

---
+++ Asteroids/main.py
@@ -0,0 +1,29 @@
+import pygame
+from constants import *
+
+
+pygame.init()
+
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+black = (0,0,0)
+
+while True:
+    screen.fill(black)
+    pygame.display.flip()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
